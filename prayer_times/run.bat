c8e8f17f38cab0863727ef3ce3a51764501ab9cf
Harizi Riyadh <hariziriyadh@gmail.com>
2026-09-20 16:23:21 +0100

    fix: resolve Windows auto-startup path and window minimize/close to tray

--- prayer_times/run.bat
+++ prayer_times/run.bat
@@ -1,7 +1,23 @@
 @echo off
 title Djazair Prayer Times
 cd /d "%~dp0"
-djazair.exe main.dz
+
+if exist "..\prayer_times.exe" (
+    start "" "..\prayer_times.exe" %*
+    exit /b 0
+)
+
+if exist "prayer_times.exe" (
+    start "" "prayer_times.exe" %*
+    exit /b 0
+)
+
+if exist "__main__.dz" (
+    djazair.exe __main__.dz %*
+) else (
+    djazair.exe main.dz %*
+)
+
 if %ERRORLEVEL% NEQ 0 (
     echo.
     echo [ERROR] An error occurred while running Djazair Prayer Times.
